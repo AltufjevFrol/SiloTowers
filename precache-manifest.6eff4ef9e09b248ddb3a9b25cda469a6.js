38bace530208db3bb288eb4338bc5e30d432c8f8
Pilipenko Artem <44920400+AltufjevFrol@users.noreply.github.com>
2019-09-12 20:27:24 +0300

    Add files via upload

---
+++ precache-manifest.6eff4ef9e09b248ddb3a9b25cda469a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c0c0301a53cab298a9f65443245bff",
+    "url": "/SiloTowers/index.html"
+  },
+  {
+    "revision": "63df55305096401cfeeb",
+    "url": "/SiloTowers/static/css/main.f083cf5a.chunk.css"
+  },
+  {
+    "revision": "cef5ba35005a8c8e6a86",
+    "url": "/SiloTowers/static/js/2.1c3c4b02.chunk.js"
+  },
+  {
+    "revision": "63df55305096401cfeeb",
+    "url": "/SiloTowers/static/js/main.69cea684.chunk.js"
+  },
+  {
+    "revision": "e1f5f1efde28be96f673",
+    "url": "/SiloTowers/static/js/runtime~main.2a7c660a.js"
+  }
+]);
